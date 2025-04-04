14001fc5643adeeeb6c1d8ec4342405872fac351
Shamil Roshan.N <shamilroshan390@gmail.com>
2025-04-05 00:29:45 +0530

    Navbar and footer is created

---
+++ client/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Landing.css'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+
+function Landing() {
+
+    return (
+
+        <>
+            
+            <Navbar />
+            <section id='landing-root'>
+
+                <p>Landing page</p>
+
+            </section>
+            <Footer />
+
+        </>
+
+    )
+
+}
+
+export default Landing
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <StrictMode>
+
+        <Router>
+
+            <App />
+
+        </Router>
+
+    </StrictMode>,
+
 )
