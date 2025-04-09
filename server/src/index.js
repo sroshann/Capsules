d0c52ae4cd15287bac6302a9894c7aef8d659530
Shamil Roshan.N <shamilroshan390@gmail.com>
2025-04-09 19:09:49 +0530

    Deployment
    Made ready to deploy

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,22 @@
 import express from 'express'
 import http from 'http'
+import path from 'path' // Deployment
+import dotenv from 'dotenv'
 
 const app = express()
 const server = http.createServer( app )
+const __dirname = path.resolve()
+dotenv.config()
+
+if( process.env.NODE_ENV === 'production' ) {
+
+    app.use( express.static( path.join( __dirname, '../client/dist' ) ) )
+    app.get('*', ( request, response ) => {
+
+        response.sendFile( path.join( __dirname, '../client', 'dist', 'index.html' ) )
+
+    })
+
+}
 
 server.listen( 5000, () => console.log('Server is running') )
