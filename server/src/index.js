bab5b979a2c5ce5463947fe33b49d97ba0034737
Shamil Roshan.N <shamilroshan390@gmail.com>
2025-08-02 07:34:55 +0530

    Changed the issue on default port

--- server/src/index.js
+++ server/src/index.js
@@ -37,7 +37,7 @@ if( process.env.NODE_ENV === 'production' ) {
 
 }
 
-server.listen( process.env.port || 5000, () => {
+server.listen( process.env.PORT || 5000, () => {
 
     console.log('Server is running')
     connectDb()
